412051c38efc45ff0b884442d54f05f256be9b34
devsk0111 <devsk0111@gmail.com>
2024-10-27 16:54:46 +0900

    exit문 연습하기

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -6,9 +6,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("== Start Dashboard ==");
-        System.out.print("명령: ");
-        String cmd = sc.nextLine();
-        System.out.printf("입력받은 명령어: %s\n", cmd);
+
+        while(true) {
+            String exit = "exit";
+            System.out.print("명령: ");
+            String cmd = sc.nextLine();
+            System.out.printf("입력받은 명령어: %s\n", cmd);
+
+            if (cmd.equalsIgnoreCase(exit)) {
+                break;
+            }
+        }
         System.out.println("== Exit Dashboard ==");
+        sc.close();
     }
 }
