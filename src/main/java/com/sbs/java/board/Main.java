f1f9da409c7c3d5288ba6b7ba53990f342b29762
devsk0111 <devsk0111@gmail.com>
2024-10-27 16:44:13 +0900

    잉여스러움

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -1,11 +1,14 @@
 package com.sbs.java.board;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
         System.out.println("== Start Dashboard ==");
-        System.out.println("치지직...");
-        System.out.println("두두둥...");
+        System.out.print("명령: ");
+        String cmd = sc.nextLine();
+        System.out.printf("입력받은 명령어: %s\n", cmd);
         System.out.println("== Exit Dashboard ==");
     }
 }
