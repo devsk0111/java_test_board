688134daeb39c4f53a8ebefb9663d763eb6170d4
devsk0111 <devsk0111@gmail.com>
2024-10-27 17:16:13 +0900

    /usr/article/write

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -8,13 +8,31 @@ public class Main {
         System.out.println("== Start Dashboard ==");
 
         while(true) {
-            String exit = "exit";
             System.out.print("명령: ");
             String cmd = sc.nextLine();
-            System.out.printf("입력받은 명령어: %s\n", cmd);
 
-            if (cmd.equalsIgnoreCase(exit)) {
+            int id = 1;
+
+            if (cmd.equalsIgnoreCase("usr/article/write")) {
+                System.out.println("== 게시물 작성 ==");
+                System.out.print("제목 : ");
+                String title = sc.nextLine();
+
+                System.out.println("내용 : ");
+                String content = sc.nextLine();
+
+                System.out.printf("%d 번째 계시물, '%s'의 내용이 저장 되었습니다\n\n", id, title);
+                id++;
+
+            }
+
+            else if (cmd.equalsIgnoreCase("exit")) {
+
                 break;
+            }
+
+            else {
+                System.out.printf("명령어: %s\n그런 명령어 없어요 ㅡ_ㅡ\n", cmd);
             }
         }
         System.out.println("== Exit Dashboard ==");
