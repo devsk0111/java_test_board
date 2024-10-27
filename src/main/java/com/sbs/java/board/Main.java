c415176eb2da2e245c7d89665c921a420726db1c
devsk0111 <devsk0111@gmail.com>
2024-10-27 18:24:21 +0900

    last detail 보기

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int lastArticleId = 0;
+        Article lastArticle = null;
         Scanner sc = new Scanner(System.in);
         System.out.println("== Start Dashboard ==");
 
@@ -22,11 +23,26 @@ public class Main {
 
                 int id = ++lastArticleId;
 
-                Article article = new Article(id, title, content); // 게시물 객체 생성
+                Article article = new Article(id, title, content);// 게시물 객체 생성
+                lastArticle = article;
 
                 System.out.printf("%d번째 계시물, \"%s\"의 내용이 저장 되었습니다\n", article.id, article.title);
                 System.out.println("생성된 게시물 객체 : " + article);
 
+            }
+
+            else if (cmd.equalsIgnoreCase("detail")) {
+              Article article = lastArticle;
+
+                if (article == null) { //유효성 검사
+                    System.out.println("게시물 존재하지 않네요..?");
+                    continue; // 밑에 내용 스킵
+                }
+                System.out.println("== 게시물 상세보기 ==");
+
+                System.out.printf("게시물 id : %d\n", article.id);
+                System.out.printf("게시물 title : %s\n", article.title);
+                System.out.printf("게시물 content : %s\n", article.content);
             }
 
             else if (cmd.equalsIgnoreCase("exit")) {
