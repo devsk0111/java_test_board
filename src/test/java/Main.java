275b8594fd3e49c991971f4302f4f1664569ac25
devsk0111 <devsk0111@gmail.com>
2024-10-28 16:38:44 +0900

    param 구별하기

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
         String queryString = "a=1&b=2&b=3";
         String[] bits = queryString.split("&");
-        System.out.println(Arrays.toString(bits));
+        System.out.println(Arrays.toString(bits)); // 파라미터
 
         for (String bit : bits) {
             System.out.println(bit);
@@ -17,6 +17,16 @@ public class Main {
         System.out.print("\n");
 
         Arrays.stream(bits).forEach(System.out::println);
+
+        System.out.print("\n");
+
+        for(String bit : bits) {
+            String[] bitBits = bit.split("=");
+            String paramName = bitBits[0];
+            String paramValue = bitBits[1];
+
+            System.out.printf("%s : %s\n", paramName, paramValue);
+        }
 
     }
 }
