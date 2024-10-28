03eb4fc5bfcea4a29a983f4e7071012378abeb7e
devsk0111 <devsk0111@gmail.com>
2024-10-28 17:00:23 +0900

    새로운 파라미터 추가할 수 있도록 코드 구현하기

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,31 +1,32 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
         // url로 &단위로 분석하게 된다.
 
-
-        String queryString = "a=1&b=2&b=3";
+        String queryString = "a=1&b=2&c=3&d=400&e=500000";
         String[] bits = queryString.split("&");
-        System.out.println(Arrays.toString(bits)); // 파라미터
 
-        for (String bit : bits) {
-            System.out.println(bit);
-        }
-
-        System.out.print("\n");
-
-        Arrays.stream(bits).forEach(System.out::println);
-
-        System.out.print("\n");
+        List<String> paramNames = new ArrayList<>();
+        List<Integer> paramValues = new ArrayList<>();
 
         for(String bit : bits) {
             String[] bitBits = bit.split("=");
             String paramName = bitBits[0];
             String paramValue = bitBits[1];
 
-            System.out.printf("%s : %s\n", paramName, paramValue);
+            paramNames.add(paramName);
+            paramValues.add(Integer.parseInt(paramValue));
+
+        }
+
+        for(int i = 0; i < paramNames.size(); i++) {
+            String paramName = paramNames.get(i);
+            int paramValue = paramValues.get(i);
+
+            System.out.printf("%s : %d\n",  paramName, paramValue);
         }
 
     }
