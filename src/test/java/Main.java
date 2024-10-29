86bd040218ba3f2b4c4f1413c4effbf86351b327
devsk0111 <devsk0111@gmail.com>
2024-10-29 19:47:16 +0900

    생성자를 통해서 구현하기

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -20,21 +20,16 @@ class Rq {
 
     Rq(String url) {
         this.url = url;
+        this.params = Util.getParamsFromUrl(url);
+        this.urlPath = Util.getUrlPathFromUrl(url);
     }
 
 
     public Map<String, String> getParams() {
-        if (params == null) {
-            params = Util.getParamsFromUrl(url);
-        }
-
         return params;
     }
 
     public String getUrlPath() {
-        if (urlPath == null) {
-            urlPath = Util.getUrlPathFromUrl(url);
-        }
         return urlPath;
     }
 }
